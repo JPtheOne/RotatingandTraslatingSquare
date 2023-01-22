2db0bb6a87a56d90f1cbd012fab725f96609956b
JPtheOne <102324051+JPtheOne@users.noreply.github.com>
2023-01-22 14:18:06 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Cubo2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.pictureBox1.Location = new System.Drawing.Point(25, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(752, 277);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(399, 324);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(271, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Rotate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(271, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Translate";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(271, 381);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 44);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Rotate in Origin";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cubo2
+{
+    public partial class Form1 : Form
+    {
+        static Bitmap bmp;
+        static Graphics g;
+        int sqsize=100;
+ 
+        Point a, b, c, d,origin,a1,b1,c1,d1,aAx,bAx,cAx,dAx;
+
+        
+
+        Point aT, bT, cT, dT;
+        Point aRO, bRO, cRO, dRO;
+        double angle;
+
+       public Form1()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            g = Graphics.FromImage(bmp);
+            pictureBox1.Image = bmp;
+
+            origin = new Point(bmp.Width / 2, bmp.Height / 2);
+            Axis();
+            a = new Point(pictureBox1.Width / 2, pictureBox1.Height / 2);
+            b = new Point((pictureBox1.Width / 2) + sqsize, pictureBox1.Height / 2);
+            c = new Point((pictureBox1.Width / 2) + sqsize, (pictureBox1.Height / 2) - sqsize);
+            d = new Point(pictureBox1.Width / 2, (pictureBox1.Height / 2) - sqsize);
+
+            Square(a, b, c, d);
+              
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            angle = Convert.ToInt32(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+
+            Rotate();
+            g.Clear(Color.White);
+
+            Axis();
+
+            Square(a1, b1, c1, d1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            angle = Convert.ToInt32(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+
+            Translate();
+            g.Clear(Color.White);
+
+            Axis();
+
+            Square(aT, bT, cT, dT);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            angle = Convert.ToInt32(textBox1.Text);
+            angle = -(angle * Math.PI / 180);
+
+            RotateinOrigin();
+            g.Clear(Color.White);
+
+            Axis();
+
+            Square(aRO, bRO, cRO, dRO);
+        }
+
+        private void Square(Point a, Point b, Point c, Point d)//Square 
+        {
+            g.DrawLine(Pens.Gray, a, b);
+            g.DrawLine(Pens.Gray, b, c);
+            g.DrawLine(Pens.Gray, c, d);
+            g.DrawLine(Pens.Gray, d, a);
+
+            pictureBox1.Invalidate();
+        }
+
+        private void Axis() //Draws the cartesian plane in the canva
+        {
+            aAx = new Point(pictureBox1.Width / 2, 0);
+            bAx = new Point(pictureBox1.Width / 2, pictureBox1.Height);
+            cAx = new Point(0, pictureBox1.Height / 2);
+            dAx = new Point(pictureBox1.Width, pictureBox1.Height / 2);
+
+            g.DrawLine(Pens.Violet,aAx,bAx);
+            g.DrawLine(Pens.Violet,cAx,dAx);
+        }
+
+        private void Rotate()
+        {
+            a1.X = (int)(((a.X - origin.X) * Math.Cos(angle)) - ((a.Y - origin.Y) * Math.Sin(angle)) + origin.X);
+            a1.Y = (int)(((a.X - origin.X) * Math.Sin(angle)) + ((a.Y - origin.Y) * Math.Cos(angle)) + origin.Y);
+            b1.X = (int)(((b.X - origin.X) * Math.Cos(angle)) - ((b.Y - origin.Y) * Math.Sin(angle)) + origin.X);
+            b1.Y = (int)(((b.X - origin.X) * Math.Sin(angle)) + ((b.Y - origin.Y) * Math.Cos(angle)) + origin.Y);
+            c1.X = (int)(((c.X - origin.X) * Math.Cos(angle)) - ((c.Y - origin.Y) * Math.Sin(angle)) + origin.X);
+            c1.Y = (int)(((c.X - origin.X) * Math.Sin(angle)) + ((c.Y - origin.Y) * Math.Cos(angle)) + origin.Y);
+            d1.X = (int)(((d.X - origin.X) * Math.Cos(angle)) - ((d.Y - origin.Y) * Math.Sin(angle)) + origin.X);
+            d1.Y = (int)(((d.X - origin.X) * Math.Sin(angle)) + ((d.Y - origin.Y) * Math.Cos(angle)) + origin.Y);
+        }
+
+        private void Translate()
+        {
+            aT.X = (int)(((a.X - origin .X) * Math.Cos(angle)) - ((a.Y - origin .Y) * Math.Sin(angle)) + origin .X);
+            aT.Y = (int)(((a.X - origin .X) * Math.Sin(angle)) + ((a.Y - origin .Y) * Math.Cos(angle)) + origin .Y);
+            bT.X = (int)(((b.X - origin .X) * Math.Cos(angle)) - ((b.Y - origin .Y) * Math.Sin(angle)) + origin .X);
+            bT.Y = (int)(((b.X - origin .X) * Math.Sin(angle)) + ((b.Y - origin .Y) * Math.Cos(angle)) + origin .Y);
+            cT.X = (int)(((c.X - origin .X) * Math.Cos(angle)) - ((c.Y - origin .Y) * Math.Sin(angle)) + origin .X);
+            cT.Y = (int)(((c.X - origin .X) * Math.Sin(angle)) + ((c.Y - origin .Y) * Math.Cos(angle)) + origin .Y);
+            dT.X = (int)(((d.X - origin .X) * Math.Cos(angle)) - ((d.Y - origin .Y) * Math.Sin(angle)) + origin .X);
+            dT.Y = (int)(((d.X - origin .X) * Math.Sin(angle)) + ((d.Y - origin .Y) * Math.Cos(angle)) + origin .Y);
+        }
+
+
+        private void RotateinOrigin()
+        {
+            aRO.X = (int)(((a.X - 50 - origin.X) * Math.Cos(angle)) - ((a.Y + 50 - origin.Y) * Math.Sin(angle)) + origin.X);
+            aRO.Y = (int)(((a.X - 50 - origin.X) * Math.Sin(angle)) + ((a.Y + 50 - origin.Y) * Math.Cos(angle)) + origin.Y);
+            bRO.X = (int)(((b.X - 50 - origin.X) * Math.Cos(angle)) - ((b.Y + 50 - origin.Y) * Math.Sin(angle)) + origin.X);
+            bRO.Y = (int)(((b.X - 50 - origin.X) * Math.Sin(angle)) + ((b.Y + 50 - origin.Y) * Math.Cos(angle)) + origin.Y);
+            cRO.X = (int)(((c.X - 50 - origin.X) * Math.Cos(angle)) - ((c.Y + 50 - origin.Y) * Math.Sin(angle)) + origin.X);
+            cRO.Y = (int)(((c.X - 50 - origin.X) * Math.Sin(angle)) + ((c.Y + 50 - origin.Y) * Math.Cos(angle)) + origin.Y);
+            dRO.X = (int)(((d.X - 50 - origin.X) * Math.Cos(angle)) - ((d.Y + 50 - origin.Y) * Math.Sin(angle)) + origin.X);
+            dRO.Y = (int)(((d.X - 50 - origin.X) * Math.Sin(angle)) + ((d.Y + 50 - origin.Y) * Math.Cos(angle)) + origin.Y);
+        }
+    }   
+ 
+       
+    
+}
